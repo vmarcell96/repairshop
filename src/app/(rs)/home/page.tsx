555cdb8e585326612e42218d3d9bce2376dca5d1
Marcell Vajda <vajdam96@gmail.com>
2025-01-12 18:53:06 +0100

    Add metadata to routes

--- src/app/(rs)/home/page.tsx
+++ src/app/(rs)/home/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Home Page",
+};
+
 export default function Home() {
   return <h2>Home page</h2>;
 }
--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Tickets",
+};
+
 export default function Tickets() {
   return <h2>Tickets page</h2>;
 }
