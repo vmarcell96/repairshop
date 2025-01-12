41e69a53cc43d9e8f5402b685c5eb3c858e64421
Marcell Vajda <vajdam96@gmail.com>
2025-01-12 20:41:59 +0100

    Initial schema for tickets and customers

---
+++ src/db/schema.ts
@@ -0,0 +1,62 @@
+import {
+  pgTable,
+  serial,
+  varchar,
+  boolean,
+  timestamp,
+  integer,
+  text,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+
+export const customers = pgTable("customers", {
+  id: serial("id").primaryKey(),
+  firstName: varchar("first_name").notNull(),
+  lastName: varchar("last_name").notNull(),
+  email: varchar("email").unique().notNull(),
+  phone: varchar("phone").unique().notNull(),
+  address1: varchar("address1").notNull(),
+  address2: varchar("address2"),
+  city: varchar("city").notNull(),
+  state: varchar("state").notNull(),
+  //max 4 digits
+  zip: varchar("zip", { length: 4 }).notNull(),
+  notes: text("notes"),
+  active: boolean("active").notNull().default(true),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    // automatically called on update
+    .$onUpdate(() => new Date()),
+});
+
+export const tickets = pgTable("customers", {
+  id: serial("id").primaryKey(),
+  customerId: integer("customer_id")
+    .notNull()
+    .references(() => customers.id),
+  title: varchar("title").notNull(),
+  description: text("description"),
+  completed: boolean("completed").notNull().default(false),
+  tech: varchar("tech").notNull().default("unassigned"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    // automatically called on update
+    .$onUpdate(() => new Date()),
+});
+
+// customers can have many tickets
+export const customersRelations = relations(customers, ({ many }) => ({
+  tickets: many(tickets),
+}));
+
+// one ticket references one customer
+export const ticketsRelations = relations(tickets, ({ one }) => ({
+  customer: one(customers, {
+    fields: [tickets.customerId],
+    references: [customers.id],
+  }),
+}));
