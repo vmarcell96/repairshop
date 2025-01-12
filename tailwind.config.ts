33effc0695eca738e1c0f66fa5ea9fbd64ba822b
Marcell Vajda <vajdam96@gmail.com>
2025-01-12 18:10:18 +0100

    Add animation for header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,28 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      keyframes: {
+        appear: {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+        },
+        slide: {
+          from: {
+            transform: "translateX(100%)",
+          },
+          to: {
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        appear: "appear 1s ease-in-out",
+        slide: "slide 750ms ease-in-out",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
