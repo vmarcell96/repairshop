8fcd295896316478c33001d0c94397609c085316
Marcell Vajda <vajdam96@gmail.com>
2025-01-12 23:16:25 +0100

    switch off logging for queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL!);
 
-const db = drizzle(sql, { logger: true });
+// const db = drizzle(sql, { logger: true });
+const db = drizzle(sql);
 
 export { db };
