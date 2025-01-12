a744f3ad860f378b6484fbfe4ad721a0d7e56cff
Marcell Vajda <vajdam96@gmail.com>
2025-01-12 18:52:55 +0100

    Add not found handling

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const metadata = {
+  title: "Page Not Found",
+};
+
+export default function NotFound() {
+  return (
+    <div className="px-2 w-full">
+      <div className="mx-auto py-4 flex flex-col justify-center items-center gap-4">
+        <h2 className="text-2xl">Page Not Found</h2>
+        <Image
+          className="m-0 rounded-xl"
+          src="/images/not-found.png"
+          width={300}
+          height={300}
+          sizes="300px"
+          alt="Page Not Found"
+          priority={true}
+          title="Page Not Found"
+        />
+        <Link href="/">Return Home</Link>
+      </div>
+    </div>
+  );
+}
