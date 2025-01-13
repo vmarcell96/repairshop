c017c97a978d8c03b595e54c9c9c5ac1a9ea2bdd
Marcell Vajda <vajdam96@gmail.com>
2025-01-13 21:56:25 +0100

    Fix typo

--- src/zod-schemas/customer.ts
+++ src/zod-schemas/customer.ts
@@ -3,7 +3,7 @@ import { customers } from "@/db/schema";
 
 export const insertCustomerSchema = createInsertSchema(customers, {
     firstName: (schema) => schema.min(1, "First name is required"),
-    lastName: (schema) => schema.min(1, "First name is required"),
+    lastName: (schema) => schema.min(1, "Last name is required"),
     address1: (schema) => schema.min(1, "Address is required"),
     city: (schema) => schema.min(1, "City is required"),
     state: (schema) => schema.length(1, "State is required"),
