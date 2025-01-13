d2492c1c06bcf214dd2d1248b1a4296bc2d80893
Marcell Vajda <vajdam96@gmail.com>
2025-01-13 21:56:49 +0100

    Create states array constant containing austrias states

---
+++ src/constants/StatesArray.ts
@@ -0,0 +1,11 @@
+export const StatesArray = [
+    { id: "B", description: "Burgenland" },
+    { id: "K", description: "Carinthia" },
+    { id: "N", description: "Lower Austria" },
+    { id: "O", description: "Upper Austria" },
+    { id: "S", description: "Salzburg" },
+    { id: "St", description: "Styria" },
+    { id: "T", description: "Tyrol" },
+    { id: "V", description: "Vorarlberg" },
+    { id: "W", description: "Vienna" },
+];
