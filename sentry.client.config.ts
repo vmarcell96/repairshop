261f317a5d245bb6a6d7b6ee7428800154e00274
Marcell Vajda <vajdam96@gmail.com>
2025-01-15 22:31:04 +0100

    Add ignoreErrors prop so redirect wont be logged

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,24 +5,25 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://eda5f8b1b3bb178b84715e53bdd6cda5@o4508631855595520.ingest.de.sentry.io/4508631859527760",
+    dsn: "https://eda5f8b1b3bb178b84715e53bdd6cda5@o4508631855595520.ingest.de.sentry.io/4508631859527760",
 
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+    // specifically matching redirects
+    ignoreErrors: [/^NEXT_REDIRECT$/],
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Add optional integrations for additional features
+    integrations: [Sentry.replayIntegration()],
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+    // Define how likely Replay events are sampled.
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 });
