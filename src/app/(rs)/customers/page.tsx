f85234bf80191e9f135b46cc40dcc18b0cad35db
Marcell Vajda <vajdam96@gmail.com>
2025-01-15 23:25:51 +0100

    use search on pages

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,25 @@
+import CustomerSearch from "./CustomerSearch";
+import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+
 export const metadata = {
-  title: "Customers",
+    title: "Customers Search",
 };
 
-export default function Customers() {
-  return <h2>Customers page</h2>;
+export default async function Customers({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+    const { searchText } = await searchParams;
+
+    if (!searchText) return <CustomerSearch />;
+
+    const results = await getCustomerSearchResults(searchText);
+
+    return (
+        <>
+            <CustomerSearch />
+            <p>{JSON.stringify(results)}</p>
+        </>
+    );
 }
--- src/app/(rs)/tickets/page.tsx
+++ src/app/(rs)/tickets/page.tsx
@@ -1,7 +1,36 @@
+import TicketSearch from "./TicketSearch";
+import { getOpenTickets } from "@/lib/queries/getOpenTickets";
+import { getTicketSearchResults } from "@/lib/queries/getTicketSearchResults";
+
 export const metadata = {
-  title: "Tickets",
+    title: "Tickets Search",
 };
 
-export default function Tickets() {
-  return <h2>Tickets page</h2>;
+export default async function Tickets({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+    const { searchText } = await searchParams;
+
+    if (!searchText) {
+        //get open tickets by default
+        const results = await getOpenTickets();
+
+        return (
+            <>
+                <TicketSearch />
+                <p>{JSON.stringify(results)}</p>
+            </>
+        );
+    }
+
+    const results = await getTicketSearchResults(searchText);
+
+    return (
+        <>
+            <TicketSearch />
+            <p>{JSON.stringify(results)}</p>
+        </>
+    );
 }
