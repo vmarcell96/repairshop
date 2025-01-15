c65dfa7eca2cb5c92ce9578162ec4cd3e6fa514f
Marcell Vajda <vajdam96@gmail.com>
2025-01-15 22:36:53 +0100

    Create searchbutton

---
+++ src/components/SearchButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { LoaderCircle } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function SearchButton() {
+    const status = useFormStatus();
+
+    return (
+        <Button type="submit" disabled={status.pending} className="w-20">
+            {status.pending ? (
+                <LoaderCircle className="animate-spin" />
+            ) : (
+                "Search"
+            )}
+        </Button>
+    );
+}
