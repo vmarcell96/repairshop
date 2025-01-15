b95f4db728ac24746d6bbfeaea608e1365ef079f
Marcell Vajda <vajdam96@gmail.com>
2025-01-15 22:31:56 +0100

    add next safe actions module

---
+++ src/lib/safe-action.ts
@@ -0,0 +1,26 @@
+import { createSafeActionClient } from "next-safe-action";
+import { z } from "zod";
+import * as Sentry from "@sentry/nextjs";
+
+export const actionClient = createSafeActionClient({
+    defineMetadataSchema() {
+        return z.object({
+            actionName: z.string(),
+        });
+    },
+    handleServerError(e, utils) {
+        console.log(e.constructor.name);
+        const { clientInput, metadata } = utils;
+        Sentry.captureException(e, (scope) => {
+            scope.clear();
+            scope.setContext("serverError", { message: e.message });
+            scope.setContext("metadata", { actionName: metadata?.actionName });
+            scope.setContext("clientInput", { clientInput });
+            return scope;
+        });
+        if (e.constructor.name === "NeonDbError") {
+            return "Database Error: Your data did not save. Support will be notified";
+        }
+        return e.message;
+    },
+});
